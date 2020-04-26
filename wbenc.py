e821145ffea9f76288401ddbca18de57f9a1770f
yanjs <felixyjs@gmail.com>
2020-04-25 23:08:28 -0400

    use argparse

--- wbenc.py
+++ wbenc.py
@@ -3,14 +3,11 @@
 
 from collections import defaultdict
 from functools import reduce
+import argparse
 import sys
 import pickle
 
-DICT = 'dict.pkl'
-INPUT = 'input.txt'
-OUTPUT = 'output.txt'
-MODE = 'ENCODE'
-VER = '0.0.1'
+VER = '0.0.2'
 DELIMITERS = ('', ';', "'", '4', '5', '6', '7', '8', '9', '0')
 
 class identitydict(dict):
@@ -34,81 +31,35 @@ def init(dict_file: str) -> tuple:
             decoding_dict[k+d] = c[1]
     return encoding_dict, decoding_dict
 
-def print_help():
-    print('WBENC v{}:'.format(VER))
-    print('SYNOPSIS')
-    print(
-    '''\twbenc\
-    [-i input_file (default: input.txt)]\
-    [-o output_file (default: output.txt)]\
-    [-d dict_file (pickle format, default: dict.pkl)])\
-    [OPTIONS]''')
-    print('OPTIONS')
-    print('\t-D: decode input_file into output_file using dict_file.')
-    print('\t-E: encode input_file into output_file using dict_file.')
-    print('\t-G: generate pickle-format dict_file with raw_dict-format input_file')
-    print('\t\traw_dict: a UTF-8 text file where each line follows the format below.')
-    print('\t\t\tWORD:str \\t ENCODE_INTO:str \\t FREQUENCY:num')
-
-def set_input(it: iter):
-    global INPUT
-    INPUT = next(it)
-
-def set_output(it: iter):
-    global OUTPUT
-    OUTPUT = next(it)
-
-def set_dict(it: iter):
-    global DICT
-    DICT = next(it)
-
-def set_decode_mode(it: iter):
-    global MODE
-    MODE = 'DECODE'
-
-def set_encode_mode(it: iter):
-    global MODE
-    MODE = 'ENCODE'
-
-def set_generate_dict_mode(it: iter):
-    global MODE
-    MODE = 'GENERATE'
-
 def main():
-    if len(sys.argv) == 1:
-        print_help()
-        return
-    argi = iter(sys.argv)
-    next(argi)
-    arg_list = {
-            '-i': set_input,
-            '-o': set_output,
-            '-d': set_dict,
-            '-D': set_decode_mode,
-            '-E': set_encode_mode,
-            '-G': set_generate_dict_mode,
-            }
-    while True:
-        try:
-            arg = next(argi)
-        except StopIteration:
-            break
-        if arg in arg_list:
-            try:
-                arg_list[arg](argi)
-            except StopIteration:
-                print_help()
-                return
+    parser = argparse.ArgumentParser(description='Encode Chinese characters')
+    parser.add_argument('-i', '--input', action='store', nargs=1,
+            default=['input.txt'], help='specify the input filename')
+    parser.add_argument('-o', '--output', action='store', nargs=1,
+            default=['output.txt'], help='specify the output filename')
+    parser.add_argument('-d', '--dict', action='store', nargs=1,
+            default=['dict.pkl'], help='specify the dict file')
+    parser.add_argument('-D', '--decode', action='store_const', const=True,
+            help='use decode mode')
+    parser.add_argument('-E', '--encode', action='store_const', const=True,
+            help='use encode mode')
+    parser.add_argument('-G', '--generate', action='store_const', const=True,
+            help='use generate dict mode')
+    args = parser.parse_args()
 
-    if MODE == 'GENERATE':
-        my_dict = init(INPUT)
-        with open(DICT, 'wb') as f:
+    input_file = args.input[0]
+    output_file = args.output[0]
+    dict_file = args.dict[0]
+
+    if args.generate:
+        my_dict = init(input_file)
+        with open(dict_file, 'wb') as f:
             pickle.dump(my_dict, f)
-    elif MODE == 'ENCODE':
-        with open(DICT, 'rb') as f:
+    elif args.encode:
+        with open(dict_file, 'rb') as f:
             encoding_dict, _= pickle.load(f)
 
-        with open(INPUT, encoding='utf-8') as f:
+        with open(input_file, encoding='utf-8') as f:
             raw = f.read()
 
         def sqeeze(a, x):
@@ -122,13 +73,13 @@ def main():
 
         out = ''.join(reduce(sqeeze, raw, None))
 
-        with open(OUTPUT, 'w', encoding='utf-8') as f:
+        with open(output_file, 'w', encoding='utf-8') as f:
             f.write(out)
-    elif MODE == 'DECODE':
-        with open(DICT, 'rb') as f:
+    elif args.decode:
+        with open(dict_file, 'rb') as f:
             _, decoding_dict = pickle.load(f)
 
-        with open(INPUT, encoding='utf-8') as f:
+        with open(input_file, encoding='utf-8') as f:
             raw = f.read()
 
         rawi = iter(raw)
@@ -160,7 +111,7 @@ def main():
 
         out = ''.join(out)
 
-        with open(OUTPUT, 'w', encoding='utf-8') as f:
+        with open(output_file, 'w', encoding='utf-8') as f:
             f.write(out)
 
 if __name__ == '__main__':
